d7f2424cf0f02713e19de314309225c5f6e428f2
Szymon Maslanka <szymon.maslanka@4pts.co.uk>
2017-07-24 11:20:58 +0200

    moved logic into separate function

--- isItPrime/ViewController.swift
+++ isItPrime/ViewController.swift
@@ -11,72 +11,38 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var numberTextField: UITextField!
-    @IBAction func isItPrime(_ sender: Any) {
-        
-        if let userEnterString = numberTextField.text {
-        
-            let userEnterInteger = Int(userEnterString)
-            
-            if let number = userEnterInteger {
-                var isPrime = true
-                
-                if number == 1 {
-                    isPrime = false
-                }
-                
-                var i = 2
-                
-                while i < number {
-                    if number % i == 0 {
-                        isPrime = false
-                    }
-                
-                    i += 1
-                }
-                if isPrime {
-                    resultLabel.text = "\(number) is prime!"
-                    
-                } else {
-                    resultLabel.text = "\(number) is not prime!"
-                }
-                
-            } else {
-                resultLabel.text = "Please enter a positive number"
-            }
-        }
-    }
-    
     @IBOutlet weak var resultLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+	
+	@IBAction func isItPrime(_ sender: Any) {
+		if let userEnterString = numberTextField.text {
+			let userEnterInteger = Int(userEnterString)
+			if let number = userEnterInteger {
+				if isItPrime(number: number) {
+					resultLabel.text = "\(number) is prime!"
+				} else {
+					resultLabel.text = "\(number) is not prime!"
+				}
+			} else {
+				resultLabel.text = "Please enter a positive number"
+			}
+		}
+	}
+	
+	
+	private func isItPrime(number: Int) -> Bool {
+		if number == 1 {
+			return false
+		}
+		
+		var i = 2
+		
+		while i < number {
+			if number % i == 0 {
+				return false
+			}
+			i += 1
+		}
+		
+		return true
+	}
 }
-
-//let number = 17
-
-//var isPrime = true
-//
-//if number == 1 {
-//    isPrime = false
-//}
-//
-//var i = 2
-//
-//while i < number {
-//    if number % i == 0 {
-//        isPrime = false
-//    }
-//
-//    i += 1
-//}
-//
-//print isPrime
-
